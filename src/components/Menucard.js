106746f37f3872682f917efe488aa9b67bfc8632
kevin-magu <wanjaukevinmagu@gmail.com>
2023-07-04 19:31:27 +0300

    u

--- src/components/Menucard.js
+++ src/components/Menucard.js
@@ -2,11 +2,15 @@ import React from 'react'
 import '../styles/Menu.css'
 function Menucard({image, name,price}) {
   return (
+    <>
     <div className="menu-card">
         <div className='menu-photo' style={{ backgroundImage: `url(${image})` }}></div>
         <div className='menu-name'>{name}</div>
         <p className='menu-price'>$ {price}</p>
+        <p className='order'>Order</p>
     </div>
+    
+    </>
   )
 }
 
