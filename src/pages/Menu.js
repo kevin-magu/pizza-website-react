127039673c883a2dc020b5930fb73ae2eb12d4cc
kevin-magu <wanjaukevinmagu@gmail.com>
2023-07-04 19:33:45 +0300

    U

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -6,7 +6,6 @@ function Menu() {
   return (
     <div className="menu-page-container">
     <h2 className="menu-title">Menu</h2>
-    <p type='submit'>ORDER</p>
     <div className="div-card-container">
         {foodInfo.map((menuItem, key) => {
           return (
