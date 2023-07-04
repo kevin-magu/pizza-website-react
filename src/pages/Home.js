b5737109ac5e7bc5be809c4095592a261062f193
kevin-magu <wanjaukevinmagu@gmail.com>
2023-07-04 22:15:26 +0300

    U

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import bgImage from '../assets/images/homepage-bg.jpg';
 function Home() {
   return (
     <div className='home' style={{ backgroundImage: `url(${bgImage})` }}>
-      <h1>Welcome to Lizzi Pizzas</h1>
+      <h1>Welcome to Lizzie's Pizzas</h1>
       <h3>Sweet to come again tommorow</h3>
     </div>
   )
