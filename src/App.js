a98a4629e5e1326be3538b3b98bc714a37c3677b
kevin-magu <wanjaukevinmagu@gmail.com>
2023-07-04 19:48:49 +0300

    U

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
+import About from './pages/About';
 import Footer from './components/Footer';
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>nuAboutContact
       <Route path= "/" element={<Home />} />
       <Route path= "/menu" element={<Menu />} />
+      <Route path= "/about" element={<About />} />
       </Routes>
      </Router>
      <Footer />
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import '../styles/About.css'
+
+function About() {
+  return (
+    <div className='about-page-container'>
+        <div className='right-side'></div>
+        <div className='left-side'></div>
+    </div>
+  )
+}
+
+export default About
